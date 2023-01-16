b6ad72930c9f6a0f44996ff7a7a117090fd03273
firecrow <fire@firecrow.com>
2023-01-16 11:31:36 +0000

    blocking out new structure for gka_entry

--- block-sound-mem/block-sound-mem.h
+++ block-sound-mem/block-sound-mem.h
@@ -4,10 +4,11 @@
 #define GKA_SUCCESS 0
 #define GKA_MEMORY_FAILURE 7
 
-typedef uint64_t gka_value;
-typedef uint32_t gka_subvalue;
-typedef double gka_decimal;
-typedef gka_subvalue gka_local_address;
+typedef uint64_t gka_value_t;
+typedef double gka_decimal_t;
+typedef uint32_t gka_subvalue_t;
+typedef uint16_t gka_operand_t;
+typedef gka_subvalue gka_local_address_t;
 typedef gka_value gka_time_t;
 
 struct gka_mem_block {
@@ -32,59 +33,32 @@ enum gka_transition_type {
   GKA_EASE_OUT,
 };
 
-enum gka_sound_flags {
-  GKA_REPEATS = 0,
-};
-
-struct gka_segment_head {
-  char operand;
-  char : 0;
-  char : 0;
-  char : 0;
-  union {
-    gka_subvalue transition;
-    gka_local_address entry_on_next_block;
-  } head_op;
-};
-
 /* This is the basic block layout that other objects will fit itno*/
 struct gka_entry {
-  struct gka_segment_head head;
-  gka_value dimension;
-  gka_value value;
-};
-
-struct gka_segment_reference {
-  gka_local_address root;
-  gka_local_address current;
-};
-
-struct gka_sound {
-  struct gka_segment_head head;
-  gka_local_address frequency;
-  gka_local_address volume;
-  gka_local_address shape;
-  gka_local_address distortion;
-};
-
-struct gka_sound_head {
-  char operand;
-  char sound_flags;
-  char : 0;
-  char : 0;
-  gka_local_address sound;
-};
-
-struct gka_sound_event {
-  struct gka_sound_head head;
-  gka_time_t start;
-  gka_time_t repeat;
-};
-
-struct gka_segment {
-  struct gka_segment_head head;
-  gka_time_t gotime;
-  gka_decimal value;
+  /* this is the set of values which determing if this is a sound, a segment, or
+   * an indication to continue at another address */
+  head {
+    /* this is the indication of what type of entry it is, uses the gka_value_operand enum*/
+    gka_operand_t type;
+    /* this is the function identifier of the function used to smooth
+     transitions such as ease, it uses values fro gka_transition_type enum */
+    gka_operand_t transition;
+    /* this is the address of the first segmetn if it is a sound, or the next
+     * segment if this indidates to continue on at another address */
+    gka_local_address_t addr;
+  };
+  union {
+    /* this struct is used if it is a sound refering to child segments*/
+    struct {
+      gka_time_t start;
+      gka_time_t repeat;
+    } schedule;
+    /* this struct is used if it is a segment*/
+    struct {
+      gka_value_t value;
+      gka_time_t start_time;
+    } placement;
+  } values;
 };
 
 gka_local_address gka_allocate_space(struct gka_mem_block *blk, size_t size);
